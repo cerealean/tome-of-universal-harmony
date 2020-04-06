e685b93c8002529063e90fdf23c1a4f28f0dd0af
Michael Crawford <cerealean@me.com>
2020-04-05 20:35:39 -0500

    playing

--- src/app/dice-roller/20-sided-dice/20-sided-dice.component.ts
+++ src/app/dice-roller/20-sided-dice/20-sided-dice.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, ViewChild, ElementRef, Input, AfterViewInit, OnChanges, SimpleChanges, OnDestroy } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef, Input, OnDestroy, AfterViewInit } from '@angular/core';
 import { range } from 'src/app/helpers/number-helpers';
 import { Observable, Subscription } from 'rxjs';
 
@@ -11,68 +11,55 @@ import { Observable, Subscription } from 'rxjs';
   templateUrl: './20-sided-dice.component.html',
   styleUrls: ['./20-sided-dice.component.scss']
 })
-export class TwentySidedDiceComponent implements OnInit, AfterViewInit, OnChanges, OnDestroy {
+export class TwentySidedDiceComponent implements OnInit, OnDestroy, AfterViewInit {
 
   @ViewChild('die') $die: ElementRef<HTMLDivElement>;
 
-  @Input() numberOfDice: number;
   @Input() rollClicked: Observable<boolean>;
 
   private rollClicked$: Subscription;
 
-  private initialSide = 1;
+  private initialSide = this.getRandomIntInclusive(1, 20);
   private animationDuration = 3000;
-
-  private diceElements: HTMLDivElement[] = [];
 
   ngOnInit() {
     this.rollClicked$ = this.rollClicked.subscribe(() => {
-      this.diceElements.forEach(die => {
-        this.rollDice(die);
-      });
+      this.rollDie();
     });
+  }
+
+  ngAfterViewInit() {
+    this.rollTo(this.initialSide);
   }
 
   ngOnDestroy() {
     this.rollClicked$.unsubscribe();
   }
 
-  ngAfterViewInit() {
-    this.diceElements = Array.from(document.querySelectorAll('div .die'));
-  }
-
-  ngOnChanges(changes: SimpleChanges) {
-    if (changes.numberOfDice) {
-      setTimeout(() => {
-        this.diceElements = Array.from(document.querySelectorAll('div .die'));
-      }, 200);
-    }
-  }
-
   getRange(num: number): number[] {
     return range(num);
   }
 
-  rollTo(dieElement: HTMLDivElement, face: number) {
-    this.setFace(dieElement, face);
+  rollTo(face: number) {
+    this.setFace(face);
   }
 
-  rollDice(dieElement: HTMLDivElement) {
+  rollDie() {
+    const dieElement = this.$die.nativeElement;
     dieElement.classList.add('rolling');
 
     setTimeout(() => {
       dieElement.classList.remove('rolling');
 
-      this.rollTo(dieElement, this.getRandomIntInclusive(1, 20));
+      this.rollTo(this.getRandomIntInclusive(1, 20));
     }, this.animationDuration);
   }
 
-  private setFace(dieElement: HTMLDivElement, face: number) {
-    dieElement.dataset.face = String(face);
+  private setFace(face: number) {
+    this.$die.nativeElement.dataset.face = String(face);
   }
 
   private getRandomIntInclusive(min: number, max: number) {
-    // const byteArray = this.getRandomCryptoArray();
     const byteArray = new Uint32Array(1);
     window.crypto.getRandomValues(byteArray);
 
